6a851e67389cee5d7792c416a15643405236d39e
Vitali Teren <tyanmyn@yandex.ru>
2023-04-07 19:12:07 +0300

    Added file OtbrosChisel.py

---
+++ OtbrosChisel.py
@@ -0,0 +1,14 @@
+a = int(input('Введите число: '))
+cikl = 0
+while True:
+    if a <= 0:
+        break
+    elif a > 0:
+        a = a // 10
+        cikl += 1
+        print(a)
+
+print('cikl =', cikl)
+b = int(input('Введите второе число: ))
+c = a + b
+print('Сумма равна: ', c)
